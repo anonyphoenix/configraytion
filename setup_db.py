ac09e4734a4b0592d6c090fdf7b1edaa8d56896a
Anonyphoenix <anonyphoenix@proton.me>
2024-10-07 21:34:01 +0330

    v0.1.1

--- setup_db.py
+++
@@ -1,8 +0,0 @@
-from sqlite3 import connect
-
-db = connect("configs.db")
-cursor = db.cursor()
-
-cursor.execute(f"CREATE TABLE IF NOT EXISTS config(url, in_channel, config_id)")
-cursor.execute(f"ALTER TABLE config ADD text {None}")
-# print(cursor.execute("SELECT * FROM config "))
