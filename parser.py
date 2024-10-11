22cacae8ecac941c872c08d6b15876444ce88f1e
Anonyphoenix <anonyphoenix@proton.me>
2024-10-11 17:36:20 +0330

    v0.1.8

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ def parse_configshub(msg):
     country_emoji = xray_config[-xray_config[::-1].index('#'):xray_config.index('t.me/')]\
         .replace('[', '').replace(']', '')
     logging.debug('country emoji extracted.')
-    xray_config = xray_config[0:-xray_config[::-1].index('#')]
+    xray_config = xray_config[0:-xray_config[::-1].rindex('#')-1]
     logging.debug('config rewritten.')
     i = msg.index('ᴄᴏᴜɴᴛʀʏ: #')
     j = msg.index('\nᴄᴏɴғɪɢsʜᴜʙ')
@@ -13,4 +13,6 @@ def parse_configshub(msg):
     logging.debug('country extracted.')
     country_abbreviation = country[country.rindex('(')+1:country.rindex(')')]
     logging.debug('country abbreviation extracted.')
+    country = country.replace(f'({country_abbreviation})', '')
+    logging.debug('country fixed.')
     return xray_config, country, country_abbreviation, country_emoji
