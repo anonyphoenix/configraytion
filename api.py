e27da87635104cd5d9b6bf7043281a0810d032a7
Anonyphoenix <anonyphoenix@proton.me>
2024-10-11 18:54:01 +0330

    v0.1.9

--- api.py
+++ api.py
@@ -17,27 +17,27 @@ tokens = db[config.MONGO_DB]['tokens']
 app = FastAPI()
 
 
-@app.get("/sub/")
+@app.get('/sub/')
 async def get_sub_link(token: str):
-    find_token = tokens.find_one({"token": token})
+    find_token = tokens.find_one({'token': token})
     if find_token is None:
         response = {
-            "status": 403,
-            "message": "Token is invalid!"
+            'status': 403,
+            'message': 'Token is invalid!'
         }
         return Response(json.dumps(response), 403)
     else:
-        token_credit = find_token["credit"]
+        token_credit = find_token['credit']
         if token_credit <= 0:
             response = {
-                "status": 403,
-                "message": "The user balance is not enough!"
+                'status': 403,
+                'message': 'The user balance is not enough!'
             }
             return Response(json.dumps(response), 403)
         else:      
-            response_text = ""
-            find_configs = configs.find().sort("config_id", DESCENDING)
-            for config in find_configs:
-                response_text += config['url'] + "\n"
-            tokens.update_one(find_token, {"$set": {"credit": int(token_credit) - 1}})
+            response_text = ''
+            find_configs = configs.find().sort('config_id', DESCENDING)
+            for c in find_configs:
+                response_text += c['url'] + '#' + c['country_emoji'] + ' ' + config.BOT_LINK + ' ' + c['country'] + '\n'
+            tokens.update_one(find_token, {'$set': {'credit': int(token_credit) - 1}})
             return Response(response_text, 200)
